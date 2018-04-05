dcfb08c5a6a72a42055ca35f5abd119b6ba461e9
eleve <augereau.louis@gmail.com>
2018-04-05 16:24:43 +0200

    Page Inscription + Titre / fixed menu

--- js/script.js
+++ js/script.js
@@ -23,17 +23,17 @@ function showDivs(n) {
 new Chart(document.getElementById("pie-chart"), {
   type: 'pie',
   data: {
-    labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+    labels: ["JS", "HTML", "CSS", "PHP"],
     datasets: [{
       label: "Population (millions)",
-      backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-      data: [2478, 5267, 734, 784, 433]
+      backgroundColor: ["#ffd633", "#1aa3ff", "#990000", "#00b33c"],
+      data: [15, 40, 35, 10]
     }]
   },
   options: {
     title: {
       display: true,
-      text: 'Predicted world population (millions) in 2050'
+      text: "Compétences Web maîtrisées en 2018"
     }
   }
 });
